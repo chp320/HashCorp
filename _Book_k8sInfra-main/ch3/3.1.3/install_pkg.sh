49ea99f192a565913dccb47cc688b226c3663971
skyfox <chp320@naver.com>
2025-01-04 19:56:11 +0900

    modified Vagrant, install_pkg.sh according to the current info.

--- _Book_k8sInfra-main/ch3/3.1.3/install_pkg.sh
+++ _Book_k8sInfra-main/ch3/3.1.3/install_pkg.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 # install packages 
-yum install epel-release -y
-yum install vim-enhanced -y
-yum install git -y
+sudo yum install epel-release -y
+sudo yum install vim-enhanced -y
+sudo yum install git -y
 
 # install docker 
-yum install docker-ce-18.06.0.ce-3.el7 docker-ce-cli-18.06.0.ce-3.el7 \
-    containerd.io-1.2.6-3.3.el7 -y
-systemctl enable --now docker
+sudo yum install docker-ce-18.06.0.ce-3.el7 docker-ce-cli-18.06.0.ce-3.el7 \
+         containerd.io-1.2.6-3.3.el7 -y
+sudo systemctl enable --now docker
 
 # install kubernetes cluster 
-yum install kubectl-$1 kubelet-$1 kubeadm-$1 -y
-systemctl enable --now kubelet
+sudo yum install kubectl-$1 kubelet-$1 kubeadm-$1 -y
+sudo systemctl enable --now kubelet
 
 # git clone _Book_k8sInfra.git 
 if [ $2 = 'Main' ]; then
